aaf65b046a1eddf757ac83b82b8b5864dc33e3e5
Benjamin <afamily54321.b@gmail.com>
2026-07-18 13:03:05 -0700

    Hardcode parkour start Y to 91

--- src/main/java/com/pathogenesis/system/ArenaPersistentState.java
+++ src/main/java/com/pathogenesis/system/ArenaPersistentState.java
@@ -33,7 +33,7 @@ public class ArenaPersistentState extends PersistentState {
         nbt.putBoolean("built", built);
         nbt.putBoolean("bossDefeated", bossDefeated);
         nbt.putBoolean("skinTerrainBuilt", skinTerrainBuilt);
-        nbt.putBoolean("parkourBuilt15", parkourBuilt);
+        nbt.putBoolean("parkourBuilt16", parkourBuilt);
         nbt.putInt("cx", cx);
         nbt.putInt("cy", cy);
         nbt.putInt("cz", cz);
@@ -45,7 +45,7 @@ public class ArenaPersistentState extends PersistentState {
         s.built = nbt.getBoolean("built");
         s.bossDefeated = nbt.getBoolean("bossDefeated");
         s.skinTerrainBuilt = nbt.getBoolean("skinTerrainBuilt");
-        s.parkourBuilt = nbt.getBoolean("parkourBuilt15");
+        s.parkourBuilt = nbt.getBoolean("parkourBuilt16");
         s.cx = nbt.getInt("cx");
         s.cy = nbt.getInt("cy");
         s.cz = nbt.getInt("cz");
--- src/main/java/com/pathogenesis/system/ParkourCourse.java
+++ src/main/java/com/pathogenesis/system/ParkourCourse.java
@@ -32,7 +32,7 @@ public class ParkourCourse {
             BlockPos pos = state.getCenter();
             Direction facing = Direction.SOUTH;
 
-            build(world, pos.getX(), pos.getY(), pos.getZ(), facing);
+            build(world, pos.getX(), 91, pos.getZ(), facing);
             state.setParkourBuilt(true);
 
             player.sendMessage(Text.literal(
